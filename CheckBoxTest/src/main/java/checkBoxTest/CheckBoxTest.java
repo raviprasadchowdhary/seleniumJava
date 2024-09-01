cc476178ecb8676b7c66a74a45e178905654e1ce
raviprasadchowdhary <78257726+raviprasadchowdhary@users.noreply.github.com>
2024-09-01 21:39:43 +0530

    checkBoxTest commit

---
+++ CheckBoxTest/src/main/java/checkBoxTest/CheckBoxTest.java
@@ -0,0 +1,27 @@
+package checkBoxTest;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class CheckBoxTest {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+		driver.findElement(By.cssSelector("input[id='checkBoxOption1']")).click();
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[id='checkBoxOption1']")).isSelected());
+		driver.findElement(By.cssSelector("input[id='checkBoxOption1']")).click();
+		Assert.assertFalse(driver.findElement(By.cssSelector("input[id='checkBoxOption1']")).isSelected());
+
+		int numberOfCheckboxes = driver.findElements(By.xpath("//input[@type='checkbox']")).size();
+		Assert.assertEquals(numberOfCheckboxes, 3);
+	}
+}
